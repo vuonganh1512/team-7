213f00bbfe51652d2c83627e2c9393c7fd6f3fc4
PSadoyan <pavelsadoyan@gmail.com>
2024-04-16 09:23:07 -0500

    Moved from numpy to pandas, can print csv files in DataFrame

--- TestSuite/test_suite.py
+++ TestSuite/test_suite.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
-
-import numpy as np
 import pandas as pd
+from pandas import DataFrame as df
 import sys
 import os
 
 def main(argv):
+  #Printing the file name and input_matrix
   input_matrix = pd.read_csv('TestSuite/InputMatrix.csv')
   print(os.getcwd())
   print(input_matrix)
 
 
+
+
 if __name__ == "__main__":
   main(sys.argv)
-
