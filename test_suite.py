69dc6e36b11c4630dd9571a0e36cf87294589626
zenarcher007 <jdouty03@icloud.com>
2024-04-16 11:54:31 -0500

    Update test_suite.py, more file readins

--- test_suite.py
+++ test_suite.py
@@ -6,9 +6,14 @@ import sys
 import os
 
 def main(argv):
-  input_matrix = pd.read_csv('TestSuite/InputMatrix.csv')
   print(os.getcwd())
+  print(os.listdir('.'))
+  input_matrix = pd.read_csv('CS322-SP2024-Core-main/FullSampleTestANDTrace/InputMatrix.csv') # Based on the formulas at time t in the input matrix
+  trace_file = pd.read_csv('CS322-SP2024-Core-main/FullSampleTestANDTrace/1.csv') # IDs correspond to the input ids of InputMatrix Evaluate upon the trace file
+  test_matrix = pd.read_csv('CS322-SP2024-Core-main/FullSampleTestANDTrace/TestMatrix.csv') # Both T and Z are given by 
   print(input_matrix)
+
+  print(trace_file)
 
 
 if __name__ == "__main__":
