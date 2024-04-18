d731de6e8a39cc9f6ac2deba42bf2f23d3a5a973
PSadoyan <pavelsadoyan@gmail.com>
2024-04-18 10:29:40 -0500

    Began incorporating user input

--- TestSuite/test_suite.py
+++ TestSuite/test_suite.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
+#from MatrixParser input MatrixParser
 import numpy as np
 import pandas as pd
+import csv
 import sys
 import os
 
 def main(argv):
-  print(os.getcwd())
-  print(os.listdir('.'))
-  input_matrix = pd.read_csv('InputMatrix.csv') # Based on the formulas at time t in the input matrix
-  trace_file = pd.read_csv('1.csv') # IDs correspond to the input ids of InputMatrix Evaluate upon the trace file
-  test_matrix = pd.read_csv('TestMatrix.csv') # Both T and Z are given by 
-  print(input_matrix)
+  input_matrix = pd.read_csv('TestSuite/InputMatrix.csv') # Based on the formulas at time t in the input matrix
+  
+  test_matrix = pd.read_csv('TestSuite/TestMatrix.csv') # Both T and Z are given by 
+  df_test = test_matrix[test_matrix.TestName == "ASE1"].Property
 
+  cur_test = input("Enter Test: ")
+  cur_input = input("Enter InputID: ")
+  time_t = input("Enter Time T: ")
+  time_z = input("Enter Time Z: ")
+  trace_file = pd.read_csv('TestSuite/' + cur_input + '.csv') # IDs correspond to the input ids of InputMatrix Evaluate upon the trace file
+
+  
   print(trace_file)
+  parse(time_t, time_z)
+  print(df_test)
 
 
 if __name__ == "__main__":
   main(sys.argv)
 
+def parse(T, Z):
+  return 0
