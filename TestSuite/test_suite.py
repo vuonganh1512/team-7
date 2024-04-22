41575de2b632ae513063300d60581bd7e4840af4
PSadoyan <pavelsadoyan@gmail.com>
2024-04-22 16:12:12 -0500

    importing Matrix Parser

--- TestSuite/test_suite.py
+++ TestSuite/test_suite.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#from MatrixParser input MatrixParser
+from MatrixParser import MatrixParser
 import numpy as np
 import pandas as pd
 import csv
@@ -9,7 +9,6 @@ import os
 
 def main(argv):
   input_matrix = pd.read_csv('TestSuite/InputMatrix.csv') # Based on the formulas at time t in the input matrix
-  
   test_matrix = pd.read_csv('TestSuite/TestMatrix.csv') # Both T and Z are given by 
   df_test = test_matrix[test_matrix.TestName == "ASE1"].Property
 
@@ -20,7 +19,7 @@ def main(argv):
   trace_file = pd.read_csv('TestSuite/' + cur_input + '.csv') # IDs correspond to the input ids of InputMatrix Evaluate upon the trace file
 
   
-  print(trace_file)
+  print(trace_file.parse(time_t, time_z))
   parse(time_t, time_z)
   print(df_test)
 
