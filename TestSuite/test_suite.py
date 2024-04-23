3c126d78e143df6331d788b1cc11bd27e59db77d
PSadoyan <pavelsadoyan@gmail.com>
2024-04-23 10:11:44 -0500

    Update test_suite.py
    Fixed bugs with MatrixParser in test_suite.py
    - Can run MatrixParser correctly now

--- TestSuite/test_suite.py
+++ TestSuite/test_suite.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from MatrixParser import MatrixParser
+from MatrixParser.MatrixParser import MatrixParser
 import numpy as np
 import pandas as pd
 import csv
@@ -8,24 +8,24 @@ import sys
 import os
 
 def main(argv):
+  
+
   input_matrix = pd.read_csv('TestSuite/InputMatrix.csv') # Based on the formulas at time t in the input matrix
   test_matrix = pd.read_csv('TestSuite/TestMatrix.csv') # Both T and Z are given by 
-  df_test = test_matrix[test_matrix.TestName == "ASE1"].Property
+  
 
   cur_test = input("Enter Test: ")
   cur_input = input("Enter InputID: ")
-  time_t = input("Enter Time T: ")
-  time_z = input("Enter Time Z: ")
-  trace_file = pd.read_csv('TestSuite/' + cur_input + '.csv') # IDs correspond to the input ids of InputMatrix Evaluate upon the trace file
+  #time_t = input("Enter Time T: ")
+  #time_z = input("Enter Time Z: ")
 
+  df_test = test_matrix[test_matrix.TestName == cur_test].Property[0]
   
-  print(trace_file.parse(time_t, time_z))
-  parse(time_t, time_z)
-  print(df_test)
+  trace_file = pd.read_csv('TestSuite/' + cur_input + '.csv') # IDs correspond to the input ids of InputMatrix Evaluate upon the trace filec
 
+  matrix = MatrixParser(trace_file)
+  print(matrix.parse(df_test, 0.0322235411435365, 0.122627636858503))
+  
 
 if __name__ == "__main__":
   main(sys.argv)
-
-def parse(T, Z):
-  return 0
