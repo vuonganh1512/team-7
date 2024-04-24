7fa20adc4f1c4c454ffd600f3f85c0dd5c39604b
PSadoyan <pavelsadoyan@gmail.com>
2024-04-24 17:21:00 -0500

    Update MatrixParser.py
    Added re.finditer MatrixParser.py

--- TestSuite/MatrixParser/MatrixParser.py
+++ TestSuite/MatrixParser/MatrixParser.py
@@ -2,13 +2,10 @@ import re
 
 class MatrixParser:
   def __init__(self, tracedf):
-    self.tracedf = tracedf
+    self.tracedf = tracedf;
 
   def setTraceData(self, dataframe):
     self.tracedf = dataframe
-
-
-  
 
   # Calculates a boolean truth of the property for the trace data given at times T and Z.
   # Assumes that the "T" expression is always on the left side of the operator, and the "Z" expression is always
@@ -22,14 +19,21 @@ class MatrixParser:
     # Assume that the iloc above returned only one dictionary
     Tdict = Tdata.to_dict(orient='records')[0] # https://stackoverflow.com/a/31324373
     Zdict = Zdata.to_dict(orient='records')[0]
+    print("t")
+    for match in re.finditer(r'{([A-Za-z|]+)}\[([A-Za-z|])+\]', property):
+      print(match)
+      print(match.groups())
+      print(match.group(1))
+      print(match.group(2))
+
 
     # Find the operator in the property string
-    op = re.search(r'([<>]|==)', property)
+    op = re.finditer(r'([<>]|==)', property)
     if op is None:
       raise Exception("No valid operator was present in the property string!")
-    op = op.group(0)
     
     # Find expressions on either side of the operator
+
     Texpr, Zexpr = tuple(property.split(op))
     
     # Clean the expressions, by replacing "{SOMEVAR}[T]" with just "SOMEVAR"
