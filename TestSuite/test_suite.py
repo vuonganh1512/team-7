8dad5b66ad5762803c1ef369cf619bfda1becc4c
zenarcher007 <jdouty03@icloud.com>
2024-05-01 23:00:31 -0500

    Updated data file name conventions, add OutputFormatter, project path variables

--- TestSuite/test_suite.py
+++ TestSuite/test_suite.py
@@ -4,39 +4,45 @@ import numpy as np
 import pandas as pd
 import sys
 import os
+from OutputFormatter import update_results_dataframe
+from MatrixParser import MatrixParser
+
+# Currently gets the path two levels above the test_suite.py
+global PROJECT_ROOT
+PROJECT_ROOT = os.path.realpath(__file__).split('/')[:-2]
+PROJECT_ROOT = '/'.join(PROJECT_ROOT)
+DATAFILES_PATH = PROJECT_ROOT + "/" + "DataFiles"
 
 def process_test_cases(data):
-    results = []
+  results = []
+  
+  # Iterate over each row in the data
+  for index, row in data.iterrows():
+    test_name = row['TestName']
+    constraints = row['Constraints']
+    property_test = row['Property']
     
-    # Iterate over each row in the data
-    for index, row in data.iterrows():
-        test_name = row['TestName']
-        constraints = row['Constraints']
-        property_test = row['Property']
-        
-       
-        numerical_columns = row[3:]
-        passed = evaluate_constraints(numerical_columns, constraints) and evaluate_property(numerical_columns, property_test)
-        
-        # Record the result of the test case
-        results.append((test_name, passed))
+     
+    numerical_columns = row[3:]
+    passed = evaluate_constraints(numerical_columns, constraints) and evaluate_property(numerical_columns, property_test)
     
-    return results
+    # Record the result of the test case
+    results.append((test_name, passed))
+  
+  return results
 
 def evaluate_constraints(numerical_columns, constraints):
-    
-    return True
+  return True
 
 def evaluate_property(numerical_columns, property_test):
-    
-    return True
+  return True
 
 
-input_matrix = pd.read_csv('TestSuite/TestMatrix.csv')
+input_matrix = pd.read_csv(f'{DATAFILES_PATH}/csv/InputMatrix/InputMatrix.csv')
 
 test_results = process_test_cases(input_matrix)
 
 # Display results
 for test_name, passed in test_results:
-    print(f"Test case '{test_name}': {'True' if passed else 'False'}")
+  print(f"Test case '{test_name}': {'True' if passed else 'False'}")
 pd.read_parquet('1__101.parquet', engine='fastparquet')
